7cec7961151ca3b411dd2abc388655b849dda3ff
Ian Bethe <ianbethe38@gmail.com>
2025-11-25 06:05:09 +0100

    initial commit

---
+++ 70044_Ian_Bethe_T3/70044_Ian_Bethe_T3/Products.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _70044_Ian_Bethe_T3
+{
+    class Products
+    {
+        public string ID;
+        public string Product_name;
+        public string Product_brand;
+        public double price;
+
+        public Products(string ID, string Product_name, string Product_brand, double price)
+        {
+            this.ID = create_id();
+            this.Product_name = Product_name;
+            this.Product_brand = Product_brand;
+            this.price = price;
+        }
+        public string get_id()
+        {
+            return ID;
+        }
+        public string create_id()
+        {
+            string result = "";
+            Random random = new Random();
+            char letter_1 = (char)random.Next('A', 'Z' + 1);
+            char letter_2 = (char)random.Next('A', 'Z' + 1);
+            int number_1 = random.Next(1000, 10000);
+            result = $"{letter_1} {letter_2}-{number_1}";
+            return result;
+        }
+    }
+}
+
---
+++ 70044_Ian_Bethe_T3/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _70044_Ian_Bethe_T3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Products p = create_products();
+            ProductsOnFile(p);
+           
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+
+        }
+        private Products create_products()
+        {
+            double price = Convert.ToDouble(textBox3.Text);
+            return new Products(label2.Text, textBox1.Text, textBox2.Text, price);
+        }
+        private void ProductsOnFile(Products p)
+        {
+            FileStream ab = new FileStream("products.txt", FileMode.Append, FileAccess.Write);
+            StreamWriter bc = new StreamWriter(ab);
+            bc.WriteLine($"{p.ID} $ {p.Product_name} $ {p.Product_brand} $ {p.price}");
+            bc.Close();
+            ab.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string button_read = "products.txt";
+            if (File.Exists(button_read))
+            {
+                using (FileStream fs = new FileStream(button_read, FileMode.Open, FileAccess.Read))
+                {
+                    using (StreamReader sr = new StreamReader(fs))
+                    {
+                        string a = sr.ReadLine();
+                        if (!string.IsNullOrEmpty(a))
+                        {
+                            string[] b = a.Split('$');
+                            if (b.Length == 4)
+                            {
+                                label2.Text = b[0].Trim();
+                                textBox1.Text = b[1].Trim();
+                                textBox2.Text = b[2].Trim();
+                                textBox3.Text = b[3].Trim();
+                            }
+                          
+                        }
+                       
+                    }
+                }
+            }
+        }
+    }
+}
